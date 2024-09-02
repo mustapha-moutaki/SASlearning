f852cae2df74ad7cb8dc45319767b520f73228ba
mustapha <mustaphastar067@gmail.com>
2024-09-02 16:12:38 +0200

    uploading c file

---
+++ saslearning.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int tableau[5] = { 784, 5, 45, -12001, 8 };
+    int somme = 0;
+
+    for (unsigned i = 0; i <= 5; ++i)
+        somme += tableau[i];
+    
+    printf("%d\n", somme);
+    somme = tableau[0];
+    printf("%d\n", somme);
+    return 0;
+}
